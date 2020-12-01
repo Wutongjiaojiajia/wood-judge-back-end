171be6ba626403602d72aa0a003a3b77dcb50465
黄家豪 <490899577@qq.com>
2020-12-01 17:05:19 +0800

    更新https

--- index.js
+++ index.js
@@ -11,28 +11,38 @@ const express=require('express');
 const app=express();
 const readFileName=require('./utils/readFilesName');
 const bodyParser = require('body-parser');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const httpsOption = {
+	key:fs.readFileSync('./cert/4855634_node.wutongjiaojiajia.cn.key'),
+	cert:fs.readFileSync('./cert/4855634_node.wutongjiaojiajia.cn.crt')
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //设置允许跨域访问该服务.
 app.all('*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Content-Type', 'application/json;charset=utf-8');
+	next();
 })
 
 //循环routes文件夹下文件，将路由容易挂载到app服务上
 readFileName('routes',(fileName)=>{
-    const basePath='./routes/';
-    fileName.forEach(item =>{
-        let fullPath=basePath+item;
-        app.use(require(fullPath));
-    });
+	const basePath='./routes/';
+	fileName.forEach(item =>{
+		let fullPath=basePath+item;
+		app.use(require(fullPath));
+	});
 })
 
+https.createServer(httpsOption,app).listen(3000,()=>{
+	console.log('running at port 3000...');
+});
 
-app.listen(3000,()=>{
-    console.log('running at port 3000...');
-})
+// app.listen(3000,()=>{
+// 	console.log('running at port 3000...');
+// })
